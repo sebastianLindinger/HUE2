cfec2f2651a9a83a4f9e58b150cd1ff83416e2fa
sebastianLindinger <seiglmeg@gmail.com>
2019-09-25 21:09:02 +0200

    finished 3. beispiel

--- NumberTesterSlindinger17/src/Main.java
+++ NumberTesterSlindinger17/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        NumberTester numberTester = new NumberTester("5\n1 6\n2 11\n3 22");
+        NumberTester numberTester = new NumberTester("5\n1 6\n2 11\n3 22\n2 7\n2 61\n2 10\n2 100\n2 33");
     }
 }
