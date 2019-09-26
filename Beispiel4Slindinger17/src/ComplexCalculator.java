844ff84019ab832525d27e0a8d3221d8c8510272
sebastianLindinger <seiglmeg@gmail.com>
2019-09-26 17:01:28 +0200

    finished class ComplexCalculator in Beispiel 4 and started to code main class

--- Beispiel4Slindinger17/src/ComplexCalculator.java
+++ Beispiel4Slindinger17/src/ComplexCalculator.java
@@ -1,2 +1,60 @@
-public class ComplexCalculator {
+public class ComplexCalculator extends AbstractCalculator{
+    public ComplexCalculator(){
+        super(
+                (x, y) -> {
+                    Number result = new Number();
+                    result.setA(x.getA() + x.getB());
+                    result.setB(y.getA() + y.getB());
+                    return result;
+                },
+                (x, y) -> {
+                    Number result = new Number();
+                    result.setA(x.getA() - x.getB());
+                    result.setB(y.getA() - y.getB());
+                    return result;
+                },
+                (x, y) -> {
+                    Number result = new Number();
+
+                    double a = (x.getA() * x.getB());
+                    double b = (x.getA() * y.getB());
+                    double c = (y.getA() * x.getB());
+                    double d = (y.getA() * y.getB()) * -1;
+
+                    result.setA(a+d);
+                    result.setB(b+c);
+                    return result;
+                },
+                (x, y) -> {
+                    Number result = new Number();
+
+                    double a1 = (x.getA() * x.getB());
+                    double b1 = (x.getA() * y.getB()) * -1;
+                    double c1 = (y.getA() * x.getB());
+                    double d1 = (y.getA() * y.getB());
+                    double a2 = (x.getB() * x.getB());
+                    double b2 = (y.getB() * y.getB());
+
+                    result.setA((a1+d1)/25);
+                    result.setB((b1+c1)/25);
+                    return result;
+                }
+        );
+    }
+
+    public Number add(Number a, Number b){
+        return add.calc(a, b);
+    }
+
+    public Number substract(Number a, Number b){
+        return substract.calc(a, b);
+    }
+
+    public Number multiply(Number a, Number b){
+        return multiply.calc(a, b);
+    }
+
+    public Number divide(Number a, Number b){
+        return divide.calc(a, b);
+    }
 }
--- Beispiel4Slindinger17/src/Main.java
+++ Beispiel4Slindinger17/src/Main.java
@@ -1,9 +1,86 @@
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        AbstractCalculator calc = new RelationalCalculator();
-        Number a = new Number(1.0, 2.0);
-        Number b = new Number(1.0, 2.0);
-        Number c = calc.add(a, b);
-        System.out.println(c.getA() + " " + c.getB());
+        Scanner scanner = new Scanner(System.in, "Windows-1252");
+        scanner.useDelimiter("\n");
+
+        int eing = -1;
+        while (eing != 4){
+            AbstractCalculator calculator;
+            System.out.println("Choose Calculator:");
+            System.out.println("1 - Relational Calculator");
+            System.out.println("2 - Vector Calculator");
+            System.out.println("3 - Complex Calculator");
+            System.out.println("4 - Exit program");
+
+            //enter calculator
+            System.out.print("> ");
+            try{
+                eing = Integer.parseInt(scanner.next());
+                if(eing < 1 || eing > 4) throw new Exception();
+            } catch (Exception e){
+                System.out.println("Invalid input [1-4]");
+            }
+
+            switch (eing) {
+                case 1:
+                    calculator = new RelationalCalculator();
+                    break;
+
+                case 2:
+                    calculator = new VectorCalculator();
+                    break;
+
+                case 3:
+                    calculator = new ComplexCalculator();
+                    break;
+            }
+
+            //enter numbers
+            while (true){
+                try {
+                    Number x = new Number();
+                    Number y = new Number();
+                    System.out.print("Enter number x a>");
+                    x.setA(Double.parseDouble(scanner.next()));
+                    System.out.print("Enter number x b>");
+                    x.setB(Double.parseDouble(scanner.next()));
+                    System.out.print("Enter number y a>");
+                    y.setA(Double.parseDouble(scanner.next()));
+                    System.out.print("Enter number y b>");
+                    y.setB(Double.parseDouble(scanner.next()));
+                    break;
+                } catch (Exception e){
+                    System.out.println("Invalid input");
+                }
+            }
+
+            if(!(eing < 1 || eing > 4)){
+                int eing2 = 5;
+                while (eing2 == 5) {
+                    System.out.println("Choose Operation:");
+                    System.out.println("1 - add");
+                    System.out.println("2 - subtract");
+                    System.out.println("3 - multiply");
+                    System.out.println("4 - divide");
+                    System.out.println("5 - enter numbers again");
+
+                    System.out.print("> ");
+                    try{
+                        eing2 = Integer.parseInt(scanner.next());
+                        if(eing2 < 1 || eing2 > 5) throw new Exception();
+                    } catch (Exception e){
+                        System.out.println("Invalid input [1-5]");
+                    }
+                }
+            }
+
+
+
+        }
     }
 }
